f4e1f893cb18f1906bd4c30f5e5b8f025e776771
phamdinhhien <hien.pd@urbox.vn>
2024-05-05 00:04:52 +0700

    feature: add vector icons

--- src/screens/home-screen/index.tsx
+++ src/screens/home-screen/index.tsx
@@ -1,10 +1,12 @@
 import {Text} from 'react-native';
 import React from 'react';
 import {Container} from '../../components/container';
+import FeathersIcon from 'react-native-vector-icons/Feather';
 export const HomeScreen = () => {
   return (
     <Container>
       <Text>hiển nè</Text>
+      <FeathersIcon name="anchor" />
     </Container>
   );
 };
